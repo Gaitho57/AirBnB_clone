16740c2ae01db483aabeb20f6bd813207f8db042
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-13 16:14:09 -0700

    update

--- models/tests/test_file_storage.py
+++ models/tests/test_file_storage.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python3
 import unittest
-import os
-import json
-from models.engine.file_storage import FileStorage
+from models import storage
 from models.base_model import BaseModel
 
+
 class TestFileStorage(unittest.TestCase):
-    def setUp(self):
-        self.storage = FileStorage()
-        self.model = BaseModel()
-        self.model.id = "test_id"
-        self.model.name = "Test Model"
-        self.model.my_number = 42
-        self.storage.new(self.model)
-        self.storage.save()
 
-    def tearDown(self):
-        if os.path.exists(FileStorage._FileStorage__file_path):
-            os.remove(FileStorage._FileStorage__file_path)
+    def test_all(self):
+        storage.new(BaseModel())
+        storage.new(BaseModel())
+        all_objs = storage.all()
+        self.assertEqual(len(all_objs), 2)
 
-    def test_all_method(self):
-        all_objs = self.storage.all()
-        self.assertTrue(isinstance(all_objs, dict))
-        self.assertIn("BaseModel.test_id", all_objs)
+    def test_new(self):
+        model = BaseModel()
+        storage.new(model)
+        self.assertIn(model.id, storage.all())
 
-    def test_new_method(self):
-        new_model = BaseModel()
-        self.storage.new(new_model)
-        self.assertIn("BaseModel." + new_model.id, self.storage.all())
+    def test_save(self):
+        model = BaseModel()
+        storage.new(model)
+        storage.save()
+        with open("file.json", "r") as f:
+            objects = json.load(f)
+            self.assertIn(model.id, objects)
 
-    def test_save_method(self):
-        with open(FileStorage._FileStorage__file_path, "r") as f:
-            saved_data = json.load(f)
-        self.assertTrue("BaseModel.test_id" in saved_data)
-        self.assertEqual(saved_data["BaseModel.test_id"]["name"], "Test Model")
 
-    def test_reload_method(self):
-        self.storage.reload()
-        all_objs = self.storage.all()
-        self.assertIn("BaseModel.test_id", all_objs)
-        self.assertEqual(all_objs["BaseModel.test_id"].name, "Test Model")
-
-if __name__ == '__main__':
-    unittest.main()
+if __name__ == "__main__":
+    unittest.main()`
