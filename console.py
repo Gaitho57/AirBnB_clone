4e5d6c503be043bdbf165b6b25c7c26962d58a82
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-13 16:26:28 -0700

    console

--- console.py
+++ console.py
@@ -1,32 +1,20 @@
 import cmd
-from models import storage
-
 
 class HBNBCommand(cmd.Cmd):
-
     prompt = "(hbnb) "
 
-    def do_quit(self, args):
+    def do_quit(self, arg):
         """Quit command to exit the program"""
-        print("Goodbye!")
-        exit()
+        return True
 
-    def do_help(self, args):
-        """Help command to get help about commands"""
-        if args:
-            if args in self.commands:
-                print(self.commands[args].__doc__)
-            else:
-                print("Unknown command: {}".format(args))
-        else:
-            print("Documented commands (type help <topic>):")
-            for command in self.commands:
-                print("\t{}".format(command))
+    def do_EOF(self, arg):
+        """Exit the program on EOF (Ctrl-D)"""
+        print()  # Print a new line before exiting
+        return True
 
     def emptyline(self):
-        """Don't execute anything when an empty line is entered"""
+        """Do nothing on an empty line"""
         pass
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     HBNBCommand().cmdloop()
