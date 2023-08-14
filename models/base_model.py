42d43b0736772603b4c44030a6359de77dc5b92b
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-14 04:23:47 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -5,24 +5,31 @@ from datetime import datetime
 
 class BaseModel:
     """
-    Base class for other models.
-    Attributes:
-id: string - uniquely identifies each instance of the BaseModel class.
-created_at: datetime - assign with the current datetime when an instance is createdupdated_at: datetime - assign with the current datetime when an instance is created and it will be updated every time you change your object
+Base class for other models.
+Attributes:
+id: string - uniquely identifies instances of the BaseModel class.
+created_at: represents the time&date when instance was created.
+updated_at: represens the time&date when instance was last-updtd.
     """
 
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        """
+        Initialize a BaseModel instance.
 
-        if kwargs is not empty:
+        Args:
+            *args: Unused.
+            **kwargs: Keyword arguments to initialize the instance with.
+        """
+        if kwargs:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                setattr(self, key, value)
                 if key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
