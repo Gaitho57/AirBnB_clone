d485e8d7b94862211ae2a90f19dd6f2c59a920d3
Celestine kibor <122092941+Celeekeybor@users.noreply.github.com>
2023-08-14 13:01:08 -0700

    State python class

--- models/state.py
+++ models/state.py
@@ -4,10 +4,4 @@ from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state.
-
-    Attributes:
-        name (str): The name of the state.
-    """
-
     name = ""
