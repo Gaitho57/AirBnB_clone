01f299b848b093432a75f2818c017dfffeebc2aa
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-14 04:13:39 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -5,25 +5,24 @@ from datetime import datetime
 
 class BaseModel:
     """
-Base class for other models.
-Attributes: id: string uniquely ids each instance of the BaseModel class.
-created_at: datetime object represents the time&date when instance ws creatd.
-updated_at:datetime object represents the time&ate when instance ws last updtd.
+    Base class for other models.
+    Attributes:
+id: string - uniquely identifies each instance of the BaseModel class.
+created_at: datetime - assign with the current datetime when an instance is createdupdated_at: datetime - assign with the current datetime when an instance is created and it will be updated every time you change your object
     """
 
-    def __init__(self, **kwargs):
-        """
-        Initialize a BaseModel instance.
-
-        Args:
-            **kwargs: Keyword arguments to initialize the instance with.
-        """
+    def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        if kwargs is not empty:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
 
     def __str__(self):
         """
