1b1ed33d58664866cd8f7c4c3556a02ae49ba9c0
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-14 04:44:30 -0700

    file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-
+from models import storage  # Import the storage variable
 
 class BaseModel:
     """
-Base class for other models.
-Attributes:
-id: string - uniquely identifies instances of the BaseModel class.
-created_at: represents the time&date when instance was created.
-updated_at: represens the time&date when instance was last-updtd.
+    Base class for other models.
+    Attributes: id, created_at, updated_at...
     """
 
     def __init__(self, *args, **kwargs):
@@ -20,38 +17,27 @@ updated_at: represens the time&date when instance was last-updtd.
             *args: Unused.
             **kwargs: Keyword arguments to initialize the instance with.
         """
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
-    def __str__(self):
-        """
-        Return a string representation of the instance.
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
-        Returns:
-            A string representation of the instance.
-        """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        # Call new(self) method on storage for new instances
+        if not kwargs:
+            storage.new(self)
 
     def save(self):
         """
         Update the updated_at attribute with the current datetime.
         """
         self.updated_at = datetime.now()
+        storage.save()  # Call save(self) method of storage
 
     def to_dict(self):
         """
         Return a dictionary representation of the instance.
-
-        Returns:
-            A dictionary representation of the instance.
         """
         dict_data = dict(
             (key, value)
@@ -62,3 +48,9 @@ updated_at: represens the time&date when instance was last-updtd.
         dict_data["created_at"] = self.created_at.isoformat()
         dict_data["updated_at"] = self.updated_at.isoformat()
         return dict_data
+
+    def __str__(self):
+        """
+        Return a string representation of the instance.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
---
+++ models/engine/file_storage.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+import json
+
+
+class FileStorage:
+    """
+    Serializes instances to a JSON file and deserializes JSON file to instances.
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        Returns the dictionary __objects.
+        """
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """
+        Sets in __objects the obj with key <obj class name>.id.
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """
+        Serializes __objects to the JSON file (__file_path).
+        """
+        data = {}
+        for key, value in FileStorage.__objects.items():
+            data[key] = value.to_dict()
+
+        with open(FileStorage.__file_path, "w") as file:
+            json.dump(data, file)
+
+    def reload(self):
+        """
+        Deserializes the JSON file to __objects.
+        """
+        try:
+            with open(FileStorage.__file_path, "r") as file:
+                data = json.load(file)
+                for key, value in data.items():
+                    class_name, obj_id = key.split(".")
+                    cls = globals()[class_name]
+                    obj = cls(**value)
+                    self.new(obj)
+        except FileNotFoundError:
+            pass
