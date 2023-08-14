5354164ae047d843bf95db7e00776dc9ce17069b
Gaitho57 <michaelgaitho10@gmail.com>
2023-08-13 18:27:15 -0700

    user

--- models/user.py
+++ models/user.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Module for the class User that inherits from the BaseModel class
+"""
+from models.base_model import BaseModel
+
 class User(BaseModel):
     """
-    Class representing a user.
+    A User class that inherits from the BaseModel class.
 
     Attributes:
-        email: The user's email address.
-        password: The user's password.
-        first_name: The user's first name.
-        last_name: The user's last name.
+        email (str): The user's email address.
+        password (str): The user's password.
+        first_name (str): The user's first name.
+        last_name (str): The user's last name.
     """
 
-    email = CharField(max_length=255)
-    password = CharField(max_length=128)
-    first_name = CharField(max_length=255)
-    last_name = CharField(max_length=255)
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
 
-    def __repr__(self):
-        """String representation of the User object."""
-        return (
-            f"<User {self.id} ({self.email}) ({self.first_name} {self.last_name})>"
-        )
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize User object.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        super().__init__(*args, **kwargs)
